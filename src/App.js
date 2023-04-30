1af4ca088fdd125d65ba03d31d834055180294f2
Pragyajain21 <118589578+Pragyajain21@users.noreply.github.com>
2023-04-30 10:27:19 +0530

    reusable_component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import CustomSelect from "./Component/CustomSelect";
+import countrydata from "./Component/countrydata.json";
 
 export default class App extends Component {
   state = {
@@ -15,14 +16,7 @@ export default class App extends Component {
     return (
       <div>
         <CustomSelect
-          data={[
-            { value: 1, lable: "India" },
-            { value: 2, lable: "China" },
-            { value: 3, lable: "UK" },
-            { value: 4, lable: "USA" },
-            { value: 5, lable: "Russia" },
-            { value: 6, lable: "Nepal" },
-          ]}
+          data={countrydata}
           value={country}
           placeholder="select Country"
           onChange={this.handleCustomSelect}
--- src/Component/CustomSelect.js
+++ src/Component/CustomSelect.js
@@ -1,24 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 export default function CustomSelect({ value, data, onChange, placeholder }) {
-  const handleChange = (event) => {
-    const { value } = event.target;
-    onChange(value);
+  const [countryId, setCountryId] = useState("");
+  const [state, setState] = useState([]);
+  const [stateId, setStateId] = useState("");
+  const [formError, setFormError] = useState("");
+  const [display, setdisplay] = useState("");
+
+  const handlecountry = (e) => {
+    const getcountryId = e.target.value;
+
+    const getStatedata = data.find(
+      (country) => country.country_id === getcountryId
+    ).states;
+    setCountryId(getcountryId);
+
+    setState(getStatedata);
+  };
+  const handlestate = (e) => {
+    const stateId = e.target.value;
+    setStateId(stateId);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (stateId === "" || countryId === "") {
+      setFormError("Please Select both country and state");
+    } else {
+      setdisplay("Country:" + countryId + "State:" + stateId);
+    }
   };
   return (
     <div
       className="custom-select"
-      style={{ display: "flex", justifyContent: "center", textAlign: "center" }}
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        textAlign: "center",
+        marginTop: 20,
+      }}
     >
-      <select value={value} className="select-box" onChange={handleChange}>
-        <option value="">{placeholder}</option>
-        {data.map((item, key) => (
-          <option key={key} value={item.value}>
-            {item.lable}
-          </option>
-        ))}
-      </select>
+      <form onSubmit={handleSubmit}>
+        <select
+          value={countryId}
+          className="select-box"
+          onChange={(e) => handlecountry(e)}
+        >
+          <option value="">{placeholder}</option>
+          {data.map((item, key) => (
+            <option key={key} value={item.country_id}>
+              {item.country_name}
+            </option>
+          ))}
+        </select>
+        <select
+          name="states"
+          className="select-state "
+          value={stateId}
+          onChange={(e) => handlestate(e)}
+        >
+          <option value="">--Select state--</option>
+          {state.map((getState, index) => (
+            <option value={getState.state_id} key={index}>
+              {getState.state_name}
+            </option>
+          ))}
+        </select>
+        <br />
+        <p style={{ color: "red", fontWeight: "bold" }}>{formError}</p>
+        <button
+          type="submit"
+          style={{ color: "white", background: "green", border: "none" }}
+        >
+          {" "}
+          Submit
+        </button>
+      </form>
+      <br />
+
+      <div>
+        <p>{display}</p>
+      </div>
     </div>
   );
 }
